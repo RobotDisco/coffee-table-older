27a17a02a056ea47377174eefbd4e64ef59f0728
Gaelan D'costa <gdcosta@gmail.com>
2015-08-07 00:45:18 -0400

    List visits, the hyperlinked RESTful way :D

--- src/clj/coffee_table/resource.clj
+++ src/clj/coffee_table/resource.clj
@@ -3,7 +3,16 @@
             [liberator.representation :refer [ring-response]]
             [coffee-table.visits :as visits]))
 
+;; a helper to create a absolute url for the entry with the given id
+(defn build-entry-url [request id]
+  (java.net.URL. (format "%s://%s:%s%s/%s"
+                (name (:scheme request))
+                (:server-name request)
+                (:server-port request)
+                (:uri request)
+                (str id))))
+
 (defresource visit-collection
   :allowed-methods [:get]
   :available-media-types ["application/json"]
-  :handle-ok (fn [ctx] (visits/list-visits)))
+  :handle-ok (fn [ctx] (map #(str (build-entry-url (ctx :request) (:id %))) (visits/list-visits))))
