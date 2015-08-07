812e9bb1ca28cfe5a189bbb8e5113d62b3ffe278
Gaelan D'costa <gdcosta@gmail.com>
2015-08-06 22:26:41 -0400

    yesql entries for visit create/list

---
+++ src/sql/visits/create.sql
@@ -0,0 +1,3 @@
+-- Create a visit
+INSERT INTO visits (cafe_name, date_visited, beverage, beverage_rating)
+VALUES (:cafe_name, :date_visited, :beverage, :beverage_rating)
---
+++ src/sql/visits/list.sql
@@ -0,0 +1,2 @@
+-- List all visits
+SELECT * FROM visits;
