9ffe883432d0c5c219ff79da9214d6bd52df078e
Gaelan D'costa <gdcosta@gmail.com>
2015-08-08 01:13:50 -0400

    We can create visits via POST

--- src/clj/coffee_table/resource.clj
+++ src/clj/coffee_table/resource.clj
@@ -1,7 +1,30 @@
 (ns coffee-table.resource
   (:require [liberator.core :refer [defresource]]
             [liberator.representation :refer [ring-response]]
-            [coffee-table.visits :as visits]))
+            [coffee-table.visits :as visits]
+            [clojure.java.io :as io]
+            [clojure.data.json :as json]))
+
+;; convert the body to a reader. Useful for testing in the repl
+;; where setting the body to a string is much simpler.
+(defn body-as-string [ctx]
+  (if-let [body (get-in ctx [:request :body])]
+    (condp instance? body
+      java.lang.String body
+      (slurp (io/reader body)))))
+
+;; For PUT and POST parse the body as json and store in the context
+;; under the given key.
+(defn parse-json [ctx key value-reader]
+  (when (#{:put :post} (get-in ctx [:request :request-method]))
+    (try
+      (if-let [body (body-as-string ctx)]
+        (let [data (json/read-str body :key-fn keyword :value-fn value-reader)]
+          [false {key data}])
+        {:message "No body"})
+      (catch Exception e
+        (.printStackTrace e)
+        {:message (format "IOException: %s" (.getMessage e))}))))
 
 ;; a helper to create a absolute url for the entry with the given id
 (defn build-entry-url [request id]
@@ -12,7 +35,22 @@
                 (:uri request)
                 (str id))))
 
+;; For PUT and POST check if the content type is json.
+(defn check-content-type [ctx content-types]
+  (if (#{:put :post} (get-in ctx [:request :request-method]))
+    (or
+     (some #{(get-in ctx [:request :headers "content-type"])}
+           content-types)
+     [false {:message "Unsupported Content-Type"}])
+    true))
+
 (defresource visit-collection
-  :allowed-methods [:get]
+  :allowed-methods [:get :post]
   :available-media-types ["application/json"]
+  :known-content-type? #(check-content-type % ["application/json"])
+  :malformed? #(parse-json % ::data visits/json-value-reader)
+  :post! #(let [id (:id (visits/create-visit<! (::data %)))]
+            {::id id})
+  :post-redirect? true
+  :location #(build-entry-url (get % :request) (get % ::id))
   :handle-ok (fn [ctx] (map #(str (build-entry-url (ctx :request) (:id %))) (visits/list-visits))))
--- src/clj/coffee_table/visits.clj
+++ src/clj/coffee_table/visits.clj
@@ -2,6 +2,12 @@
   (require [coffee-table.db :refer [db-config]])
   (require [yesql.core :refer [defquery]]))
 
+(defn json-value-reader [key value]
+  (cond
+    (= key :date_visited) (java.sql.Date/valueOf value)
+    (= key :beverage_rating) (Integer/parseInt value)
+    :else value))
+
 (defquery list-visits "visits/list.sql" {:connection db-config})
 
 (defquery create-visit<! "visits/create.sql" {:connection db-config})
