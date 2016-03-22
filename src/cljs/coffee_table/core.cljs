ae0c6b25d05ba9b47b8ab31f83ec80893cb516fb
Gaelan D'costa <gdcosta@gmail.com>
2016-03-21 22:46:26 -0400

    Render summary page to start.

--- src/cljs/coffee_table/core.cljs
+++ src/cljs/coffee_table/core.cljs
@@ -1,9 +1,20 @@
 (ns coffee-table.core
   (:require [goog.dom :as gdom]
             [om.next :as om]
-            [coffee-table.component.MobileSummaryItem :refer [MobileSummaryItem]]
+            [coffee-table.component.MobileSummaryList :refer [MobileSummaryList]]
             [cljs-time.core :as time]))
 
-(def msi (om/factory MobileSummaryItem))
+(def app-state (atom {:visits [{:name "FKA Twigs Cafe"
+                                :date (time/now)
+                                :beverage-rating 3
+                                }
+                               {:name "Grimes Bistro"
+                                :date (time/now)
+                                :beverage-rating 4}
+                               {:name "Tango Palace"
+                                :date (time/now)
+                                :beverage-rating 5}]}))
 
-(js/ReactDOM.render (msi) (gdom/getElement "app"))
+(def reconciler (om/reconciler {:state app-state}))
+
+(om/add-root! reconciler MobileSummaryList (gdom/getElement "app"))
