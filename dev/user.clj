df1cac5881273b6b91d9c1e2965f6d4ee6c371ee
Gaelan D'costa <gdcosta@gmail.com>
2016-06-12 20:51:25 -0400

    User tools for prepping DB

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,15 @@
 (ns user
   (:require [system.repl :refer [system init start stop go reset]]
-            [coffee-table.systems :refer [dev-system]]))
+            [coffee-table.systems :refer [dev-system]]
+            [clojure.java.io :as io]
+            [datomic.api :as datomic]))
 
 (system.repl/set-init! #'dev-system)
+
+(def schema
+  (delay
+   (read-string
+    (slurp (io/resource "data/schema.edn")))))
+
+(defn create-schema [conn]
+  (datomic/transact conn @schema))
