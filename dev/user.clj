453f82e25ed9473916ca9de26e97d708a04bf1ad
Gaelan D'costa <gdcosta@gmail.com>
2016-07-19 20:42:45 -0400

    Refactor helper method for seeding initial data

--- dev/user.clj
+++ dev/user.clj
@@ -2,29 +2,16 @@
   (:require [system.repl :refer [system init start stop go reset]]
             [coffee-table.systems :refer [dev-system]]
             [clojure.java.io :as io]
+            [coffee-table.datomic :refer [datomic-uri]]
             [datomic.api :as datomic]))
 
 (system.repl/set-init! #'dev-system)
-
-(def schema
-  (delay
-   (read-string
-    (slurp (io/resource "data/schema.edn")))))
 
 (def seed
   (delay
    (read-string
     (slurp (io/resource "data/seed.edn")))))
 
-(defn create-schema [conn]
-  (datomic/transact conn @schema))
-
-(defn create-seed [conn]
-  (datomic/transact conn @seed))
-
-(defn create-db []
-  (let [uri  "datomic:mem://coffee-table"
-        _    (datomic/create-database uri)
-        conn (datomic/connect uri)]
-    (create-schema conn)
-    (create-seed conn)))
+(defn seed-db []
+  (let [conn (datomic/connect datomic-uri)]
+    (datomic/transact conn @seed)))
